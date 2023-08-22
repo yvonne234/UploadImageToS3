817862a8686726757dba660a42862ef494de66f2
R. K. Gupta <rkguptaaa@gmail.com>
2023-08-22 22:03:17 +0530

    Update HelloWorld.vue
    endpoint change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://taw0yxrvr1.execute-api.us-east-2.amazonaws.com/default/project-Serverless'
+const API_ENDPOINT = 'https://73kt3miy69.execute-api.us-east-1.amazonaws.com/default/project-sl-function'
 
 export default {
   name: 'app',
