fd6188c6fc3c01292dc56a64f5a559307d481e35
R. K. Gupta <rkguptaaa@gmail.com>
2024-07-17 21:47:53 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://xk62vkjpu4.execute-api.us-east-1.amazonaws.com/default/ToraSaveImageFunction'
+const API_ENDPOINT = 'https://aalb9r0m59.execute-api.us-east-1.amazonaws.com/default/projServerlessFunction'
 
 export default {
   name: 'app',
